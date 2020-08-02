4284f4de51de19f31dceb32d91b47ddd986f2aee
Samyak S Sarnayak <samyak201@gmail.com>
2020-08-03 00:00:39 +0530

    [MINOR] Added a setup.py file for creating python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="browser-history", # Replace with your own username
+    version="0.0.1",
+    author="Samyak S Sarnayak",
+    author_email="samyak201@gmail.com",
+    description="A python module to extract browser history",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Samyak2/browser-history",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
