d0292ffb588da15b35c6d332048ce43e8300808d
Samyak S Sarnayak <samyak201@gmail.com>
2020-08-11 20:08:37 +0530

    [TESTS] Test more functions in Linux Firefox test
    should improve coverage

--- tests/test_browsers.py
+++ tests/test_browsers.py
@@ -1,4 +1,5 @@
 import datetime
+from pathlib import Path
 
 from .context import browser_history
 from .utils import become_linux, change_homedir # pylint: disable=unused-import
@@ -9,6 +10,15 @@ def test_firefox_linux(become_linux, change_homedir):
     """Test history is correct on Firefox for Linux"""
     f = browser_history.browsers.Firefox()
     his = f.history()
+    assert len(his) == 1
+    assert his == [(datetime.datetime(2020, 8, 3, 0, 29, 4,
+                                      tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
+                                                               'IST')),
+                    'https://www.mozilla.org/en-US/privacy/firefox/')]
+    profs = f.profiles()
+    his_path = f.history_path_profile(profs[0])
+    assert his_path == Path.home() / '.mozilla/firefox/profile/places.sqlite'
+    his = f.history_profiles(profs)
     assert len(his) == 1
     assert his == [(datetime.datetime(2020, 8, 3, 0, 29, 4,
                                       tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
