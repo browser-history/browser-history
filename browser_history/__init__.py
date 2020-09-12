1b1f95d0ae617ed41e12a83ae5ab6626346a4c98
Rahul Makhija <rmrahulmakhija74@gmail.com>
2020-09-12 14:57:04 +0530

    [CODE] Added function to generate histories for all supported browsers Issue #8
    Function get_history() added in utils.py, also imported the function in __init__.py

--- browser_history/__init__.py
+++ browser_history/__init__.py
@@ -1,1 +1,2 @@
 from . import browsers
+from browser_history.utils import get_history
--- browser_history/utils.py
+++ browser_history/utils.py
@@ -4,7 +4,8 @@ Module defines Platform class enumerates the popular Operating Systems.
 """
 import enum
 import platform
-
+from browser_history import browsers
+from browser_history import generic
 class Platform(enum.Enum):
     """An enum used to indicate the system's platform
 
@@ -37,3 +38,45 @@ def get_platform():
     if system == "Windows":
         return Platform.WINDOWS
     raise NotImplementedError(f"Platform {system} is not supported yet")
+
+def get_history():
+    """This method is used to obtain browser histories of all available and supported
+    browsers for the system platform.
+
+    :return: Object of class :py:class:`browser_history.generic.Outputs` with the
+    data member entries set to list(tuple(:py:class:`datetime.datetime`, str))
+    
+    :rtype: :py:class:`browser_history.generic.Outputs`
+    """
+    system = get_platform()
+    output_object = generic.Outputs()
+
+    if system == Platform.LINUX:
+        chrome = browsers.Chrome()
+        chrome_object = chrome.fetch()
+        output_object.entries.extend(chrome_object.entries)
+        firefox = browsers.Firefox()
+        firefox_object = firefox.fetch()
+        output_object.entries.extend(firefox_object.entries)
+        chromium = browsers.Chromium()
+        chromium_object = chromium.fetch()
+        output_object.entries.extend(chromium_object.entries)
+
+
+    elif system == Platform.WINDOWS:
+        chrome = browsers.Chrome()
+        chrome_object = chrome.fetch()
+        output_object.entries.extend(chrome_object.entries)
+        firefox = browsers.Firefox()
+        firefox_object = firefox.fetch()
+        output_object.entries.extend(firefox_object.entries)
+
+    elif system == Platform.MAC:
+        safari = browsers.Safari()
+        safari_object = safari.fetch()
+        output_object.entries.extend(safari_object.entries)
+    
+    output_object.entries.sort()
+
+    return output_object
+
