51a5fd257cbcddf335a14e2290214913b564b745
Samyak S Sarnayak <samyak201@gmail.com>
2020-09-18 20:14:13 +0530

    [DOC][CLI] add some comments to the CLI code

--- browser_history/cli.py
+++ browser_history/cli.py
@@ -2,18 +2,21 @@ import sys
 import argparse
 from browser_history import generic, browsers, utils
 
+# get list of all implemented browser by finding subclasses of generic.Browser
 available_browsers = ', '.join(b.__name__ for b in generic.Browser.__subclasses__())
-parser = argparse.ArgumentParser(description='''A tool to retrieve history from
-                                             (almost) any browser on (almost) any platform''',
+
+parser = argparse.ArgumentParser(description='''
+                                    A tool to retrieve history from
+                                    (almost) any browser on (almost) any platform''',
                                  epilog='''
                                     Checkout the GitHub repo https://github.com/pesos/browser-history
                                     if you have any issues or want to help contribute''')
+
 parser.add_argument('-b', '--browser',
                     default='all',
                     help=f'''
                         browser to retrieve history from. Should be one of all, {available_browsers}.
-                        Default is all (gets history from all browsers).
-                    ''')
+                        Default is all (gets history from all browsers).''')
 
 def main():
     args = parser.parse_args()
@@ -23,6 +26,7 @@ def main():
 
     else:
         try:
+            # gets browser class by name (string). TODO: make it case-insensitive
             browser_class = getattr(browsers, args.browser)
         except AttributeError:
             print(f'Browser {args.browser} is unavailable. Check --help for available browsers')
@@ -36,4 +40,5 @@ def main():
             sys.exit(1)
 
     for date, url in outputs.get():
+        # comma-separated output. NOT a CSV file
         print(f'{date},{url}')
