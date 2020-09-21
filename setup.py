57945a8f60e30d6ab7f5f499065894dd12c12115
Samyak S Sarnayak <samyak201@gmail.com>
2020-09-21 19:26:20 +0530

    [RELEASE] bump version to v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="browser-history", # Replace with your own username
-    version="0.1.1",
+    version="0.2.0",
     author="Samyak S Sarnayak",
     author_email="samyak201@gmail.com",
     description="A python module to extract browser history",
