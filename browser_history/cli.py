d1d41c29c1ed33a3376fcd8fac792e9ed29c9d52
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2020-09-23 00:32:02 +0530

    [FEATURE] Added option to write to output file.

--- browser_history/cli.py
+++ browser_history/cli.py
@@ -3,10 +3,13 @@ command line interface of browser-history."""
 
 import sys
 import argparse
+import csv
+import datetime
 from browser_history import generic, browsers, utils
-
 # get list of all implemented browser by finding subclasses of generic.Browser
-AVAILABLE_BROWSERS = ', '.join(b.__name__ for b in generic.Browser.__subclasses__())
+AVAILABLE_BROWSERS = ', '.join(
+    b.__name__ for b in generic.Browser.__subclasses__())
+
 
 def make_parser():
     """Creates an ArgumentParser, configures and returns it.
@@ -18,18 +21,26 @@ def make_parser():
     parser_ = argparse.ArgumentParser(description='''
                                             A tool to retrieve history from
                                             (almost) any browser on (almost) any platform''',
-                                         epilog='''
+                                      epilog='''
                                             Checkout the GitHub repo https://github.com/pesos/browser-history
                                             if you have any issues or want to help contribute''')
 
     parser_.add_argument('-b', '--browser',
-                            default='all',
-                            help=f'''
+                         default='all',
+                         help=f'''
                                 browser to retrieve history from. Should be one of all, {AVAILABLE_BROWSERS}.
                                 Default is all (gets history from all browsers).''')
+    parser_.add_argument('-o', '--output',
+                         default=str(datetime.datetime.today()),
+                         help='''
+                            Output data to file instead of terminal.
+                            Takes boolean value
+                        ''')
     return parser_
 
+
 parser = make_parser()
+
 
 def main():
     """Entrypoint to the command-line interface (CLI) of browser-history.
@@ -51,7 +62,8 @@ def main():
                     break
             browser_class = getattr(browsers, selected_browser)
         except AttributeError:
-            print(f'Browser {args.browser} is unavailable. Check --help for available browsers')
+            print(
+                f'Browser {args.browser} is unavailable. Check --help for available browsers')
             sys.exit(1)
 
         try:
@@ -60,7 +72,20 @@ def main():
         except AssertionError as e:
             print(e)
             sys.exit(1)
+    if not args.output:
+        for date, url in outputs.get():
+            # comma-separated output. NOT a CSV file
+            print(f'{date},{url}')
+    else:
+        values = []
 
-    for date, url in outputs.get():
-        # comma-separated output. NOT a CSV file
-        print(f'{date},{url}')
+        for date, url in outputs.get():
+            values.append([str(date), str(url)])
+
+        filename = args.output
+
+        with open(filename, 'w') as csvfile:
+
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['DATE', 'URL'])
+            csvwriter.writerow(values)
