50ab22f80b7da116d14fbc5e422a7305ab2ead4a
lucasace <tauro.royston@gmail.com>
2020-10-10 16:02:52 +0530

    [CODE][TESTS] Fixed tests with updated function names

--- tests/test_browsers.py
+++ tests/test_browsers.py
@@ -9,31 +9,30 @@ from .utils import become_linux, become_windows, become_mac, change_homedir # py
 def test_firefox_linux(become_linux, change_homedir):
     """Test history is correct on Firefox for Linux"""
     f = browser_history.browsers.Firefox()
-    output = f.fetch()
-    his = output.get()
+    output = f.fetch_history()
+    his = output.histories
     assert len(his) == 5
     assert his[0] == (datetime.datetime(2020, 8, 3, 0, 29, 4,
                                       tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
                                                                'IST')),
                     'https://www.mozilla.org/en-US/privacy/firefox/')
-    profs = f.profiles()
+    profs = f.profiles(f.history_file)
     his_path = f.history_path_profile(profs[0])
     assert his_path == Path.home() / '.mozilla/firefox/profile/places.sqlite'
-    his = f.history_profiles(profs).get()
+    his = f.history_profiles(profs).histories
     assert len(his) == 5
     assert his[0] == (datetime.datetime(2020, 8, 3, 0, 29, 4,
                                         tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
                                                                  'IST')),
                       'https://www.mozilla.org/en-US/privacy/firefox/')
 
-
 def test_firefox_windows(become_windows, change_homedir):
     """Test history is correct on Firefox for Windows"""
     f = browser_history.browsers.Firefox()
-    output = f.fetch()
-    his = output.get()
+    output = f.fetch_history()
+    his = output.histories
     assert len(his) == 8
-    profs = f.profiles()
+    profs = f.profiles(f.history_file)
     assert len(profs) == 2
     # the history list is long so just check the first and last item
     assert his[0] == (datetime.datetime(2020, 10, 1, 11, 43, 35,
@@ -45,7 +44,7 @@ def test_firefox_windows(become_windows, change_homedir):
                                                                   'E. Africa Standard Time')),
                        'https://www.reddit.com/')
     # get history for second profile
-    his = f.history_profiles(["Profile 2"]).get()
+    his = f.history_profiles(["Profile 2"]).histories
     assert his == [(datetime.datetime(2020, 10, 4, 14, 2, 14,
                                       tzinfo=datetime.timezone(datetime.timedelta(seconds=10800),
                                                                'E. Africa Standard Time')),
@@ -55,8 +54,8 @@ def test_firefox_windows(become_windows, change_homedir):
 def test_edge_windows(become_windows, change_homedir):
     """Test history is correct for Edge on Windows"""
     e = browser_history.browsers.Edge()
-    output = e.fetch()
-    his = output.get()
+    output = e.fetch_history()
+    his = output.histories
     # test history from all profiles
     assert len(his) == 4
     assert his == [(datetime.datetime(2020, 9, 23, 10, 22, 37,
@@ -77,12 +76,12 @@ def test_edge_windows(become_windows, change_homedir):
                     'https://pesos.github.io/')]
 
     # test history from specific profile
-    profs = e.profiles()
+    profs = e.profiles(e.history_file)
     assert len(profs) == 2
     his_path = e.history_path_profile("Profile 2")
     assert his_path == Path.home() / \
                        'AppData/Local/Microsoft/Edge/User Data/Profile 2/History'
-    his = e.history_profiles(["Profile 2"]).get()
+    his = e.history_profiles(["Profile 2"]).histories
     assert len(his) == 1
     assert his == [(datetime.datetime(2020, 9, 23, 10, 45, 3,
                                       tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
@@ -93,8 +92,8 @@ def test_safari_mac(become_mac, change_homedir):
     '''Test history is correct for Safari on macOS'''
 
     e = browser_history.browsers.Safari()
-    output = e.fetch()
-    his = output.get()
+    output = e.fetch_history()
+    his = output.histories
     assert len(his) == 5
     assert his[0] == (datetime.datetime(2020, 9, 29, 23, 34, 28,
                                         tzinfo=datetime.timezone(datetime.timedelta(seconds=19800),
