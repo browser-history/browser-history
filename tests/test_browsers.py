3ffe68d3ec1ad97752560f19082fae1d55ad5d36
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-10-30 18:26:13 +0000

    [TESTS] Fix #90 by using timezone-aware comparison via dateutil.tz

--- tests/test_browsers.py
+++ tests/test_browsers.py
@@ -9,6 +9,44 @@ from .utils import (  # noqa: F401; pylint: disable=unused-import
     change_homedir,
 )
 
+# Note: from Python >= 3.9 it is possible to use a built-in module, zoneinfo,
+# (https://docs.python.org/3/library/zoneinfo.html) to achieve timezone
+# awareness. For earlier versions, an external module must be used. Choose:
+from dateutil import tz
+
+
+# Instead setting this to the standard library timezone option:
+#     datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+# will cause failures for timezones e.g. 'Europe/London'
+# or 'Australia/Melbourne' due to DST effects.
+LOCAL_TIMEZONE = tz.gettz()
+
+
+def _detatch_timezone_stamp(hist, is_lone_item=True):
+    """Remove the timezone stamp from a datetime.datetime object.
+
+    This is so datetimes can be tested for correctness despite timezone
+    complications. Without removing the timezone stamp, we are comparing
+    objects like the following, where the timezones are equivalent but named
+    differently due to the means of extracting them:
+
+        datetime.datetime(<datetime tuple>, tzinfo=datetime.timezone(
+            datetime.timedelta(seconds=<delta>), 'AEDT'))
+
+        datetime.datetime(<datetime tuple>, tzinfo=tzfile('/etc/localtime')
+
+    but the datetime tuple should be, and is tested to be, the same in
+    both cases.
+    """
+    if is_lone_item:
+        return (hist[0].replace(tzinfo=None), hist[1])
+    else:
+        removed_tz_entries = []
+        for entry in hist:
+            removed_tz_entries.append((entry[0].replace(tzinfo=None), entry[1]))
+        return removed_tz_entries
+
+
 # pylint: disable=redefined-outer-name,unused-argument
 
 
@@ -18,7 +56,7 @@ def test_firefox_linux(become_linux, change_homedir):  # noqa: F811
     output = f.fetch_history()
     his = output.histories
     assert len(his) == 5
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             8,
@@ -27,7 +65,9 @@ def test_firefox_linux(become_linux, change_homedir):  # noqa: F811
             29,
             4,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=19800), "IST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://www.mozilla.org/en-US/privacy/firefox/",
     )
     profs = f.profiles(f.history_file)
@@ -35,7 +75,7 @@ def test_firefox_linux(become_linux, change_homedir):  # noqa: F811
     assert his_path == Path.home() / ".mozilla/firefox/profile/places.sqlite"
     his = f.history_profiles(profs).histories
     assert len(his) == 5
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             8,
@@ -44,17 +84,20 @@ def test_firefox_linux(become_linux, change_homedir):  # noqa: F811
             29,
             4,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=19800), "IST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://www.mozilla.org/en-US/privacy/firefox/",
     )
 
-def test_chrome_linux(become_linux, change_homedir):
+
+def test_chrome_linux(become_linux, change_homedir):  # noqa: F811
     """Test history is correct on Chrome for Linux"""
     f = browser_history.browsers.Chrome()
     output = f.fetch_history()
     his = output.histories
     assert len(his) == 2
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             10,
@@ -63,7 +106,9 @@ def test_chrome_linux(become_linux, change_homedir):
             34,
             30,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=7200), "CEST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "www.github.com",
     )
     profs = f.profiles(f.history_file)
@@ -71,7 +116,7 @@ def test_chrome_linux(become_linux, change_homedir):
     assert his_path == Path.home() / ".config/google-chrome/History"
     his = f.history_profiles(profs).histories
     assert len(his) == 2
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             10,
@@ -80,18 +125,20 @@ def test_chrome_linux(become_linux, change_homedir):
             34,
             30,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=7200), "CEST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "www.github.com",
     )
 
 
-def test_chromium_linux(become_linux, change_homedir):
+def test_chromium_linux(become_linux, change_homedir):  # noqa: F811
     """Test history is correct on Chromium for Linux"""
     f = browser_history.browsers.Chromium()
     output = f.fetch_history()
     his = output.histories
     assert len(his) == 2
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             10,
@@ -100,7 +147,9 @@ def test_chromium_linux(become_linux, change_homedir):
             34,
             30,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=7200), "CEST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "www.github.com",
     )
     profs = f.profiles(f.history_file)
@@ -111,7 +160,7 @@ def test_chromium_linux(become_linux, change_homedir):
     ]
     his = f.history_profiles(profs).histories
     assert len(his) == 2
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             10,
@@ -120,7 +169,9 @@ def test_chromium_linux(become_linux, change_homedir):
             34,
             30,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=7200), "CEST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "www.github.com",
     )
 
@@ -134,7 +185,7 @@ def test_firefox_windows(become_windows, change_homedir):  # noqa: F811
     profs = f.profiles(f.history_file)
     assert len(profs) == 2
     # the history list is long so just check the first and last item
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             10,
@@ -145,10 +196,12 @@ def test_firefox_windows(become_windows, change_homedir):  # noqa: F811
             tzinfo=datetime.timezone(
                 datetime.timedelta(seconds=10800), "E. Africa Standard Time"
             ),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://www.youtube.com/",
     )
-    assert his[-1] == (
+    assert _detatch_timezone_stamp(his[-1]) == (
         datetime.datetime(
             2020,
             10,
@@ -159,12 +212,14 @@ def test_firefox_windows(become_windows, change_homedir):  # noqa: F811
             tzinfo=datetime.timezone(
                 datetime.timedelta(seconds=10800), "E. Africa Standard Time"
             ),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://www.reddit.com/",
     )
     # get history for second profile
     his = f.history_profiles(["Profile 2"]).histories
-    assert his == [
+    assert _detatch_timezone_stamp(his, is_lone_item=False) == [
         (
             datetime.datetime(
                 2020,
@@ -177,7 +232,9 @@ def test_firefox_windows(become_windows, change_homedir):  # noqa: F811
                     datetime.timedelta(seconds=10800),
                     "E. Africa Standard Time",
                 ),
-            ),
+            )
+            .astimezone(LOCAL_TIMEZONE)
+            .replace(tzinfo=None),
             "https://www.reddit.com/",
         )
     ]
@@ -190,7 +247,7 @@ def test_edge_windows(become_windows, change_homedir):  # noqa: F811
     his = output.histories
     # test history from all profiles
     assert len(his) == 1
-    assert his == [
+    assert _detatch_timezone_stamp(his, is_lone_item=False) == [
         (
             datetime.datetime(
                 2020,
@@ -202,7 +259,9 @@ def test_edge_windows(become_windows, change_homedir):  # noqa: F811
                 tzinfo=datetime.timezone(
                     datetime.timedelta(seconds=19800), "India Standard Time"
                 ),
-            ),
+            )
+            .astimezone(LOCAL_TIMEZONE)
+            .replace(tzinfo=None),
             "https://pesos.github.io/",
         ),
     ]
@@ -217,7 +276,7 @@ def test_edge_windows(become_windows, change_homedir):  # noqa: F811
     )
     his = e.history_profiles(["Profile 2"]).histories
     assert len(his) == 1
-    assert his == [
+    assert _detatch_timezone_stamp(his, is_lone_item=False) == [
         (
             datetime.datetime(
                 2020,
@@ -229,7 +288,9 @@ def test_edge_windows(become_windows, change_homedir):  # noqa: F811
                 tzinfo=datetime.timezone(
                     datetime.timedelta(seconds=19800), "India Standard Time"
                 ),
-            ),
+            )
+            .astimezone(LOCAL_TIMEZONE)
+            .replace(tzinfo=None),
             "https://pesos.github.io/",
         )
     ]
@@ -242,7 +303,7 @@ def test_safari_mac(become_mac, change_homedir):  # noqa: F811
     output = e.fetch_history()
     his = output.histories
     assert len(his) == 5
-    assert his[0] == (
+    assert _detatch_timezone_stamp(his[0]) == (
         datetime.datetime(
             2020,
             9,
@@ -251,11 +312,13 @@ def test_safari_mac(become_mac, change_homedir):  # noqa: F811
             34,
             28,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=19800), "IST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://www.apple.com/in/",
     )
     assert his[1][1] == "https://www.google.co.in/?client=safari&channel=mac_bm"
-    assert his[4] == (
+    assert _detatch_timezone_stamp(his[4]) == (
         datetime.datetime(
             2020,
             9,
@@ -264,17 +327,19 @@ def test_safari_mac(become_mac, change_homedir):  # noqa: F811
             35,
             8,
             tzinfo=datetime.timezone(datetime.timedelta(seconds=19800), "IST"),
-        ),
+        )
+        .astimezone(LOCAL_TIMEZONE)
+        .replace(tzinfo=None),
         "https://pesos.github.io/",
     )
 
 
-def test_opera_windows(become_windows, change_homedir):
+def test_opera_windows(become_windows, change_homedir):  # noqa: F811
     o = browser_history.browsers.Opera()
     outputs = o.fetch_history()
     his = outputs.histories
     assert len(his) == 2
-    assert his == [
+    assert _detatch_timezone_stamp(his, is_lone_item=False) == [
         (
             datetime.datetime(
                 2020,
@@ -286,7 +351,9 @@ def test_opera_windows(become_windows, change_homedir):
                 tzinfo=datetime.timezone(
                     datetime.timedelta(seconds=10800), "E. Africa Standard Time"
                 ),
-            ),
+            )
+            .astimezone(LOCAL_TIMEZONE)
+            .replace(tzinfo=None),
             "https://www.youtube.com/",
         ),
         (
@@ -300,7 +367,9 @@ def test_opera_windows(become_windows, change_homedir):
                 tzinfo=datetime.timezone(
                     datetime.timedelta(seconds=10800), "E. Africa Standard Time"
                 ),
-            ),
+            )
+            .astimezone(LOCAL_TIMEZONE)
+            .replace(tzinfo=None),
             "https://github.com/",
         ),
     ]
