bb7e6cdacd4833c0b8dd230bda2a8af06d70ec60
Samyak S Sarnayak <samyak201@gmail.com>
2021-01-08 21:02:03 +0530

    [TESTS] test calls CLI function, uses test homedirs
    Instead of using subprocesses, CLI function is called directly
    
    This required quite a few changes as the entire thing was written
    for subprocesses
    
    Also, added fixtures to perform the tests on all platforms with the
    help of test_homedirs. This ensures that the test suite is indepedent
    of the tester's system and works even on platforms with no browsers
    (such as a CI server). It also makes the tests a LOT faster on systems
    where the browser history is huge (such as mine:))

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,20 @@
 import csv
 import itertools
 import json
-import subprocess
 import tempfile
 
 import pytest
 
 from browser_history import get_browsers
+from browser_history.cli import cli, AVAILABLE_BROWSERS
+from .utils import (  # noqa: F401
+    become_linux,
+    become_mac,
+    become_windows,
+    change_homedir,
+)
 
+# pylint: disable=redefined-outer-name,unused-argument
 
 CMD_ROOT = "browser-history"
 
@@ -61,13 +68,26 @@ GENERAL_INVALID_ARGS = [
 HELP_SIGNATURE = (
     "usage: browser-history " "[-h] [-t TYPE] [-b BROWSER] [-f FORMAT] [-o OUTPUT]"
 )
-HISTORY_TITLE = "history:\n"
-BOOKMARKS_TITLE = "bookmarks:\n"
 HISTORY_HEADER = "Timestamp,URL"
 BOOKMARKS_HEADER = "Timestamp,URL,Title,Folder"
 
-CSV_HISTORY_HEADER = HISTORY_TITLE + HISTORY_HEADER
-CSV_BOOKMARKS_HEADER = BOOKMARKS_TITLE + BOOKMARKS_HEADER
+CSV_HISTORY_HEADER = HISTORY_HEADER
+CSV_BOOKMARKS_HEADER = BOOKMARKS_HEADER
+
+
+platform_fixture_map = {
+    "linux": become_linux,
+    "windows": become_windows,
+    "mac": become_mac,
+}
+all_platform_fixtures = tuple("become_" + plat for plat in platform_fixture_map.keys())
+
+
+@pytest.fixture
+def platform(request):
+    """Fixture to change platform based on pytest parameter"""
+    request.getfixturevalue("change_homedir")
+    return request.getfixturevalue(request.param)
 
 
 def _get_browser_available_on_system():
@@ -84,63 +104,66 @@ def _get_browser_available_on_system():
     return  # None indicating no browsers available on system
 
 
-def test_no_argument():
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+def test_no_argument(capsys, platform):
     """Test the root command gives basic output."""
-    output = subprocess.check_output(CMD_ROOT, shell=True)
-    assert CSV_HISTORY_HEADER in output.decode("utf-8")
+    cli([])
+    captured = capsys.readouterr()
+    assert CSV_HISTORY_HEADER in captured.out
 
 
-def test_type_argument():
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+def test_type_argument(capsys, platform):
     """Test arguments for the type option."""
     for type_opt in VALID_CMD_OPTS[1]:
         for type_arg in VALID_TYPE_ARGS:
-            output = subprocess.check_output(
-                " ".join([CMD_ROOT, type_opt, type_arg]), shell=True
-            ).decode("utf-8")
+            cli([type_opt, type_arg])
+            captured = capsys.readouterr()
             if type_arg == "history":
-                assert output.startswith(HISTORY_TITLE)
+                # assuming csv is default
+                assert captured.out.startswith(CSV_HISTORY_HEADER)
             if type_arg == "bookmarks":
-                assert output.startswith(BOOKMARKS_TITLE)
+                assert captured.out.startswith(CSV_BOOKMARKS_HEADER)
 
 
 @pytest.mark.parametrize("browser_arg", VALID_BROWSER_ARGS)
-def test_browser_argument(browser_arg):
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+def test_browser_argument(browser_arg, capsys, platform):
     """Test arguments for the browser option."""
     for browser_opt in VALID_CMD_OPTS[2]:
-        output = subprocess.Popen(
-            " ".join([CMD_ROOT, browser_opt, browser_arg]),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True,
-        )
-        stdout, stderr = output.communicate()
-        if output.returncode == 0:
-            assert CSV_HISTORY_HEADER in stdout.decode("utf-8")
-        elif any(
-            browser_unavailable_err in stderr.decode("utf-8")
-            for browser_unavailable_err in (
-                "browser is not supported",
-                "browser is not installed",
-            )
-        ):
-            # In case the tester does not have access to the browser
-            # in question, which makes the command fail, but in a way
-            # that is expected and gives a recognised error message.
-            pytest.skip(
-                "Unable to test against {} browser because it is "
-                "not available locally".format(browser_opt)
-            )
-        else:  # command fails for another reason i.e. a test failure
-            pytest.fail(
-                "{} browser is available but the command to fetch "
-                "the history from it has failed.".format(browser_opt)
-            )
+        try:
+            cli([browser_opt, browser_arg])
+            captured = capsys.readouterr()
+            assert CSV_HISTORY_HEADER in captured.out
+        except AssertionError as e:
+            if any(
+                browser_unavailable_err in e.args[0]
+                for browser_unavailable_err in (
+                    "browser is not supported",
+                    "browser is not installed",
+                )
+            ):
+                # In case the tester does not have access to the browser
+                # in question, which makes the command fail, but in a way
+                # that is expected and gives a recognised error message.
+                pytest.skip(
+                    "Unable to test against {} browser because it is "
+                    "not available locally".format(browser_opt)
+                )
+            else:  # command fails for another reason i.e. a test failure
+                pytest.fail(
+                    "{} browser is available but the command to fetch "
+                    "the history from it has failed.".format(browser_opt)
+                )
 
 
-def test_format_argument():
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+def test_format_argument(capsys, platform):
     """Tests arguments for the format option."""
     # First check format of default:
-    csv_output = subprocess.check_output(CMD_ROOT, shell=True).decode("utf-8")
+    cli([])
+    captured = capsys.readouterr()
+    csv_output = captured.out
     # Sniffer determines format, less intensive than reading in csv.reader
     # and we don't mind the CSV dialect, so just check call doesn't error
     read_csv = csv.Sniffer()
@@ -152,39 +175,36 @@ def test_format_argument():
 
     for fmt_opt in VALID_CMD_OPTS[3]:
         for fmt_arg in VALID_FORMAT_ARGS:
-            output = subprocess.check_output(
-                " ".join([CMD_ROOT, fmt_opt, fmt_arg]), shell=True
-            ).decode("utf-8")
+            cli([fmt_opt, fmt_arg])
+            output = capsys.readouterr().out
             if fmt_arg in ("csv", "infer"):  # infer gives csv if no file
                 read_csv.sniff(output, delimiters=",")
                 assert read_csv.has_header(output)
                 assert CSV_HISTORY_HEADER in output
             elif fmt_arg == "json":
-                assert output.startswith(HISTORY_TITLE)
-                # Need to strip header off before checking for valid json
-                json.loads(output.lstrip(HISTORY_TITLE))
-            else:  # i.e. fmt_arg == 'jsonl':
-                assert output.startswith(HISTORY_TITLE)
-                jsonl_output = output.lstrip(HISTORY_TITLE)
+                json.loads(output)
+            elif fmt_arg == "jsonl":
                 # Newline-delimited json so test each line is valid json
-                for line in jsonl_output.splitlines():
+                for line in output.splitlines():
                     json.loads(line)
 
 
-def test_output_argument():
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+def test_output_argument(capsys, platform):
     """Test arguments for the output option."""
     for output_opt in VALID_CMD_OPTS[4]:
         with tempfile.NamedTemporaryFile(suffix=".csv") as f:
-            output = subprocess.check_output(
-                " ".join([CMD_ROOT, output_opt, f.name]), shell=True
-            )
+            cli([output_opt, f.name])
+            output = capsys.readouterr().out
             # Check output was not sent to STDOUT since should go to file
-            assert HISTORY_HEADER not in output.decode("utf-8")
-            with open(f.name, "rb") as f:  # now check the file
-                assert HISTORY_HEADER in f.read().decode("utf-8")
+            assert HISTORY_HEADER not in output
+            with open(f.name, "rt") as f:  # now check the file
+                assert HISTORY_HEADER in f.read()
 
 
-def test_argument_combinations():
+@pytest.mark.parametrize("platform", all_platform_fixtures, indirect=True)
+@pytest.mark.parametrize("browser", AVAILABLE_BROWSERS.split(", "))
+def test_argument_combinations(capsys, platform, browser):
     """Test that combinations of optional arguments work properly."""
     # Choose some representative combinations. No need to test every one.
     indices = (0, 1)
@@ -192,57 +212,71 @@ def test_argument_combinations():
     # To test combos of short or long option variants
     for index_a, index_b in itertools.product(indices, indices):
         if available_browser:
-            chrome_bookmarks_output = subprocess.check_output(
-                " ".join(
+            try:
+                cli(
                     [
-                        CMD_ROOT,
                         VALID_CMD_OPTS[1][index_a],  # type
                         VALID_TYPE_ARGS[1],  # ... is bookmarks,
                         VALID_CMD_OPTS[2][index_a],  # browser
-                        available_browser,  # ... is any usable on system
+                        browser,  # ... is any usable on system
                     ]
-                ),
-                shell=True,
-            ).decode("utf-8")
-            assert chrome_bookmarks_output.startswith(BOOKMARKS_TITLE)
-            assert not chrome_bookmarks_output.startswith(HISTORY_TITLE)
+                )
+            except AssertionError as e:
+                if any(
+                    browser_unavailable_err in e.args[0]
+                    for browser_unavailable_err in (
+                        "browser is not supported",
+                        "browser is not installed",
+                        "Bookmarks are not supported",
+                    )
+                ):
+                    # In case the tester does not have access to the browser
+                    # in question, which makes the command fail, but in a way
+                    # that is expected and gives a recognised error message.
+                    pytest.skip(
+                        "Unable to test against {} browser because it is "
+                        "not available locally".format(browser)
+                    )
+                else:  # command fails for another reason i.e. a test failure
+                    pytest.fail(
+                        "{} browser is available but the command to fetch "
+                        "the history from it has failed.".format(browser)
+                    )
+            output = capsys.readouterr().out
+
             read_csv = csv.Sniffer()
-            read_csv.sniff(chrome_bookmarks_output, delimiters=",")
-            assert read_csv.has_header(chrome_bookmarks_output)
+            read_csv.sniff(output, delimiters=",")
+            assert read_csv.has_header(output)
         else:  # unlikely but catch just in case can't use any browser
             for _ in range(3):  # to cover all three assert tests above
                 pytest.skip("No browsers available to test with")
         with tempfile.NamedTemporaryFile(suffix=".csv") as f:
             # This command should write to the given output file:
-            subprocess.check_output(
-                " ".join(
-                    [
-                        CMD_ROOT,
-                        VALID_CMD_OPTS[3][index_b],  # format
-                        VALID_FORMAT_ARGS[2],  # ... is json,
-                        VALID_CMD_OPTS[4][index_b],  # output
-                        f.name,  # ... is a named file
-                    ]
-                ),
-                shell=True,
-            ).decode("utf-8")
+            cli(
+                [
+                    VALID_CMD_OPTS[3][index_b],  # format
+                    VALID_FORMAT_ARGS[2],  # ... is json,
+                    VALID_CMD_OPTS[4][index_b],  # output
+                    f.name,  # ... is a named file
+                ]
+            )
             with open(f.name, "rb") as f:
                 json.loads(f.read().decode("utf-8"))
 
 
-def test_help_option():
+def test_help_option(capsys):
     """Test the command-line help provided to the user on request."""
     for help_opt in VALID_CMD_OPTS[0]:
-        output = subprocess.check_output(
-            " ".join([CMD_ROOT, help_opt]), shell=True
-        ).decode("utf-8")
-        assert HELP_SIGNATURE in output
+        with pytest.raises(SystemExit) as e:
+            cli([help_opt])
+            output = capsys.readouterr()
+            assert HELP_SIGNATURE in output
+            assert e.value.code == 0
 
 
-def test_invalid_options():
+def test_invalid_options(capsys):
     """Test that invalid options error correctly."""
     for bad_opt in INVALID_CMD_OPTS:
-        with pytest.raises(subprocess.CalledProcessError):
-            subprocess.check_output(" ".join([CMD_ROOT, bad_opt]), shell=True).decode(
-                "utf-8"
-            )
+        with pytest.raises(SystemExit) as e:
+            cli([bad_opt])
+            assert e.value.code == 2
