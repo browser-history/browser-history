3a608a17e26c140a14dbbfbc022734a608227b20
Samyak S Sarnayak <samyak201@gmail.com>
2021-12-16 19:28:48 +0530

    [RELEASE] bump version to 0.3.2 🎉

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020-2021, Samyak Sarnayak"
 author = "Samyak Sarnayak"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.3.2"
 
 
 # -- General configuration ---------------------------------------------------
