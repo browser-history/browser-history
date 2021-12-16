4023532efb55ff552ce09b96f186f1d6209713ea
Samyak S Sarnayak <samyak201@gmail.com>
2021-12-16 20:13:12 +0530

    [DOC] Use xelatex engine for sphinx latex

--- docs/conf.py
+++ docs/conf.py
@@ -58,3 +58,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #  html_static_path = ['_static']
+
+# -- LaTeX options -----------------------------------------------------------
+
+# for better unicode support (CLI ASCII art uses unicode symbols)
+# see: latex_engine = 'xelatex'
+latex_engine = "xelatex"
