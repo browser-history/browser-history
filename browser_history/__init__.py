98a27aff5456b259fa436fbd2cabd633fa9ad4b9
Samyak S Sarnayak <samyak201@gmail.com>
2024-04-20 16:04:09 +0530

    [RELEASE] bump version to 0.4.1 🎉

--- browser_history/__init__.py
+++ browser_history/__init__.py
@@ -1,6 +1,6 @@
 from . import browsers, generic, utils  # noqa: F401
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 def get_history():
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020-2021, Samyak Sarnayak"
 author = "Samyak Sarnayak"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.4.1"
 
 
 # -- General configuration ---------------------------------------------------
